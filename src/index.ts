e8f18151fdfe75cf5454b93e303755727a68db90
Michal Cieslar <michalovskyyy@gmail.com>
2023-08-30 20:49:31 +0200

    add validator

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('hello');
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+yargs(hideBin(process.argv))
+  .command(
+    '$0',
+    'Count lines of code in given file/directory.',
+    () => {},
+    (argv) => {
+      const input = argv['i'] as string;
+
+      const exclude = argv['e'] as string;
+
+      console.log();
+    },
+  )
+  .option('i', { alias: 'input', describe: 'Directory/file to count lines in', type: 'string', demandOption: true })
+  .option('e', {
+    alias: 'exclude',
+    describe: 'Directories/files to be excluded from counting',
+    type: 'array',
+    demandOption: false,
+  })
+  .help(true).argv;
